be0c2c6a459787e8c5e8e2133d418456618691be
Username 2.0 <76968547+SomeoneNamedAnya@users.noreply.github.com>
2024-02-29 12:17:10 +0300

    Update mystack.h

--- my_stack/include/mystack.h
+++ my_stack/include/mystack.h
@@ -2,25 +2,90 @@
 #include <iostream>
 #include <memory.h>
 #include <algorithm>
+#include <exception>
+
 namespace StackLib{
+    template <typename T>
     class Stack {
         public:
-            Stack();
-            ~Stack();
 
-            Stack(const Stack&);
-            Stack(const Stack&&);
+            Stack() {
+                size_ = 0;
+                capacity_ = 1;
+                stack_ = (T *)safe_calloc(1, sizeof(T));
+            }
 
-            Stack& operator=(const Stack& other);
-            Stack& operator=(Stack&& other);
+            ~Stack() {
+                free(stack_);
+            }
 
-            void Pop();
-            void Push(int);
-            int Top();
+            Stack(const Stack<T>& other) {
+                size_ = other.size_;
+                capacity_ = other.capacity_;
+                stack_ = (T*)safe_calloc(1, sizeof(T));
+                memcpy(stack_, other.stack_, sizeof(T) * capacity_);        
+            }
+
+            Stack(const Stack<T>&& other) {
+                size_ = std::move(other.size_);
+                capacity_ = std::move(other.capacity_);
+                std::move(other.stack_, other.stack_ + other.capacity_, stack_); 
+            }
+
+            Stack<T>& operator=(const Stack<T>& other) {
+                size_ = other.size_;
+                capacity_ = other.capacity_;
+                stack_ = (T*)safe_calloc(1, sizeof(T));
+                memcpy(stack_, other.stack_, sizeof(T) * capacity_);
+                return *this;
+            }
+            
+            Stack<T>& operator=(Stack<T>&& other) {
+                size_ = std::move(other.size_);
+                capacity_ = std::move(other.capacity_);
+                std::move(other.stack_, other.stack_ + other.capacity_, stack_); 
+                return *this;
+            }
+
+            void Pop() {
+                if (size_ == 0) {
+                    throw "Stack is empty";
+                }
+                size_ -= 1;
+                return;
+            }
+
+            void Push(T num) {
+                if (size_ == capacity_) {
+                    capacity_ *= 2;
+                    stack_ = (T *)safe_realloc(stack_, capacity_, sizeof(T));
+                }
+                stack_[size_] = num;
+                size_ += 1;
+            }
+
+            T Top() {
+                if (size_ == 0) {
+                    throw "Stack is empty";
+                }
+                return stack_[size_ - 1];
+            }
+
+            bool IsEmpty() {
+                return (int)size_ == 0;
+            }
 
         private:
             int size_;
             int capacity_;
-            int * stack_;
+            T * stack_;
+
+            void * safe_calloc(std::size_t num, std::size_t size) {
+                return calloc(num, size);
+            }
+
+            void * safe_realloc(T * ptr, std::size_t num, std::size_t size) {
+                return realloc(ptr, num * size);
+            }
     };
 }
