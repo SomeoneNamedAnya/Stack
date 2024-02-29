d85ac355847d06e4c76e5c3e1298401fdf1a29ec
Username 2.0 <76968547+SomeoneNamedAnya@users.noreply.github.com>
2024-02-29 12:49:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
+#include <string>
 #include "mystack.h"
 using namespace StackLib;
 
-void f(Stack q) {
+/*void f(Stack<int> q) {
     return;
-}
+}*/
 int main(){
-    Stack my_stack;
+    /*Stack <int> my_stack;
     my_stack.Push(1);
     my_stack.Push(2);
 
-    /*std::cout << my_stack.Top() << "\n";
+    std::cout << my_stack.Top() << "\n";
     my_stack.Pop();
     std::cout << my_stack.Top() << "\n";
     my_stack.Pop();
     my_stack.Push(3);
     std::cout << my_stack.Top() << "\n";
-    */
-    Stack second, third;
+    
+    Stack<int> second, third;
     second = my_stack;
     third = std::move(my_stack);
     std::cout << second.Top() << "\n";
 
     std::cout << third.Top() << "\n";
-    f(second);
-    std::cout << second.Top() << "\n";
+   // f(second);
+    std::cout << second.Top() << "\n";*/
+    Stack<std::string> str_stack;
+    str_stack.Push("qwe");
+    str_stack.Push("aaa");
+    str_stack.Push("qqq");
+
+    while (!str_stack.IsEmpty()) {
+        std::cout << str_stack.Top() << "\n";
+        str_stack.Pop();
+    }
+    str_stack.Pop();
     return 0;
 }
